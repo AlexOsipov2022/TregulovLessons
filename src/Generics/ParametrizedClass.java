e4f9b8fef3bc1d8632740358598247ed92de47ed
alexandr-osipov <alexandr.osipov@idfinance.com>
2025-01-07 13:40:13 +0300

    TregulovLessons

--- src/Generics/ParametrizedClass.java
+++ src/Generics/ParametrizedClass.java
@@ -4,20 +4,17 @@ public class ParametrizedClass {
     public static void main(String[] args) {
 
         Info<String> info1 = new Info<>("Privet!");
-
         System.out.println(info1);
-
+        Info<Integer> info2 = new Info<>(25);
+        System.out.println(info2);
     }
 }
     class Info<T> {
-
         private T value;
-
         public Info(T value) {
             this.value = value;
         }
-
         public String toString() {
-            return "{{__" + value + "__}}";
+            return "--> " + value + " <--";
         }
     }
