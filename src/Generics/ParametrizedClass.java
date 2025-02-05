e68b82ad2acb6b9e7f4a05f6afcbc06b6a8e1649
alexandr-osipov <alexandr.osipov@idfinance.com>
2025-02-05 19:39:21 +0300

    TregulovLessons

--- src/Generics/ParametrizedClass.java
+++ src/Generics/ParametrizedClass.java
@@ -5,8 +5,13 @@ public class ParametrizedClass {
 
         Info<String> info1 = new Info<>("Privet!");
         System.out.println(info1);
+        String s = info1.getValue();
+        System.out.println(s);
+
         Info<Integer> info2 = new Info<>(25);
         System.out.println(info2);
+        Integer i = info2.getValue();
+        System.out.println(i);
     }
 }
     class Info<T> {
@@ -16,5 +21,8 @@ public class ParametrizedClass {
         }
         public String toString() {
             return "--> " + value + " <--";
+        }
+        public T getValue() {
+            return value;
         }
     }
---
+++ src/Streams/Filter.java
@@ -0,0 +1,93 @@
+package Streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Filter {
+    public static void main(String[] args) {
+        Student st1 = new Student("Ivan", 'm', 22, 3, 8.3);
+        Student st2 = new Student("Nik", 'm', 28, 2, 6.3);
+        Student st3 = new Student("Elena", 'f', 19, 1, 8.9);
+        Student st4 = new Student("Petr", 'm', 35, 4, 7);
+        Student st5 = new Student("Maria", 'f', 23, 3, 7.3);
+        List<Student> students = new ArrayList<>();
+        students.add(st1);
+        students.add(st2);
+        students.add(st3);
+        students.add(st4);
+        students.add(st5);
+
+        students =  students.stream().filter(element -> element.getAge() > 22 && element.getAvgGrade() < 7.2).collect(Collectors.toList());
+        System.out.println(students);
+    }
+}
+class Student {
+    private String name;
+    private char sex;
+    private int age;
+    private int course;
+    private double avgGrade;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public void setAvgGrade(double avgGrade) {
+        this.avgGrade = avgGrade;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public double getAvgGrade() {
+        return avgGrade;
+    }
+
+
+    public Student(String name, char sex, int age, int course, double avgGrade) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.avgGrade = avgGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", course=" + course +
+                ", avgGrade=" + avgGrade +
+                '}';
+    }
+}
